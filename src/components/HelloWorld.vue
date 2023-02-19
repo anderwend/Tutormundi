98c42d7731a236feb64529d40cca55dfc3d24638
Anderson Wendell <handersonwendell.s@gmail.com>
2023-02-19 10:34:31 -0300

    adição do axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,43 +15,41 @@ defineProps({
     <v-row>
         <v-col cols="2" v-for="index3 in rating" :key="index3">
           <div @click = "changerat(index3)">
-          <img src="../assets/estrela_cheia.svg" alt="Full star icon"  >
-      </div>
-    </v-col>
-    <v-col cols="2" v-for="index2 in 5-rating" :key="index2">
-      <div @click = "changerat(index2+rating)">
-      <img src="../assets/estrela_vazia.svg" alt="Empty star icon">
-      </div> 
-    </v-col>
-        </v-row>
+            <img src="../assets/estrela_cheia.svg" alt="Full star icon"  >
+          </div>
+        </v-col>
+        <v-col cols="2" v-for="index2 in 5-rating" :key="index2">
+          <div @click = "changerat(index2+rating)">
+            <img src="../assets/estrela_vazia.svg" alt="Empty star icon">
+          </div> 
+        </v-col>
+      </v-row>
   </div>
   <v-row>
     <v-col cols="2">
-
     </v-col>
     <v-col cols="3">
-    <v-btn class="btn" style="color:#2CC6D0" variant="flat"
-    >
-      Pular
-    </v-btn>
+      <v-btn class="btn" style="color:#2CC6D0" variant="flat">
+        Pular
+      </v-btn>
     </v-col>
     <v-col cols="6">
-    <v-btn class="btn" style="color:#FFF" rounded="pill"
-      :color="rating > 0 ? '#2CC6D0': '#C8C8C8'"
-      :disabled="rating > 0 ? false : true"
-      @click="sendrat()"
-    >
-      Confirmar
-    </v-btn>
+      <v-btn class="btn" style="color:#FFF" rounded="pill"
+        :color="rating > 0 ? '#2CC6D0': '#C8C8C8'"
+        :disabled="rating > 0 ? false : true"
+        @click="sendrat()"
+        >
+        Confirmar
+      </v-btn>
     </v-col>
     <v-col cols="1"/>
   </v-row>
 </template>
 
 <script>
+
 import StarRating from 'vue-star-rating';
-
-
+import axios from 'axios';
 export default {
   data: () => ({ rating: 0, }),
   components: {
@@ -60,7 +58,11 @@ export default {
   
   methods: {
     sendrat() {
-      console.log(this.rating);
+  const estrelas = { rating: this.rating };
+  axios.post("http://localhost:8085/api/v1/avaliacao", estrelas)
+    .then((response) => {
+      alert("voce enviou "+response.data+" estrelas.");
+    });
     },
     changerat(index){
       this.rating = index;
@@ -68,9 +70,6 @@ export default {
     }
   }
 }
-
-
-
 
 </script>
 
@@ -87,13 +86,11 @@ h1 {
   
   width: 292px;
   height: 50px;
- 
   font-family: 'Londrina Solid', cursive;
   font-style: normal;
   font-weight: 400;
   font-size: 40px;
   line-height: 42px;
-  
   letter-spacing: 0.2px;
   color: #000000;
 }
